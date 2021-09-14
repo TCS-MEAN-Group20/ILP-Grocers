e7e8edcdb9331bbb96b366835f4617c4e36cc54f
johngrogan98 <60016314+johngrogan98@users.noreply.github.com>
2021-09-14 11:09:33 -0500

    added emp dash skeleton

--- Frontend/grocers-app/src/app/employee-dash/employee-dash.component.ts
+++ Frontend/grocers-app/src/app/employee-dash/employee-dash.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
-import { EmpLoginService } from '../emp-login.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-employee-dash',
@@ -10,13 +9,56 @@ import { EmpLoginService } from '../emp-login.service';
 })
 export class EmployeeDashComponent implements OnInit {
   
-  user?:string
+  reqRef = new FormGroup({
+    product: new FormControl(),
+    action: new FormControl()
+  })
+
+  orderRef = new FormGroup({
+    order: new FormControl(),
+    status: new FormControl()
+  })
+
+  blockedRef = new FormGroup({
+    user: new FormControl()
+  })
+
+  changeRef = new FormGroup({})
+
+  msg?:string
+  empUname?:string
   constructor(
-    public activateRoute:ActivatedRoute
+    public activateRoute:ActivatedRoute,
+    public router:Router
   ) { }
 
   ngOnInit(): void {
-    this.activateRoute.params.subscribe(data=>this.user=data.user);
+    this.activateRoute.params.subscribe(data=>this.empUname=data.user);
+  }
+
+  sendReq(){
+    let request = this.reqRef.value
+    console.log(request)
+
+    this.reqRef.reset()
+  }
+
+  updateOrder(){
+    let order = this.orderRef.value
+    console.log(order)
+
+    this.orderRef.reset()
+  }
+
+  unblockUser(){
+    let user = this.blockedRef.value
+    console.log(user);
+
+    this.blockedRef.reset()
+  }
+
+  changePassword(){
+    this.router.navigate(["changePassword",this.empUname])
   }
 
 
