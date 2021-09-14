a3ef7be98127a8be0508e39d13fd6e95ccff925b
Robert Ciesielski <robalb234@gmail.com>
2021-09-14 15:33:35 -0500

    Added log-in landing

--- Frontend/grocers-app/src/app/app-routing.module.ts
+++ Frontend/grocers-app/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { UsersMainComponent } from './users-main/users-main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'app-users-main', component: UsersMainComponent },
+  { path: '', component: HomeComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Frontend/grocers-app/src/app/app.module.ts
+++ Frontend/grocers-app/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { UsersMainComponent } from './users-main/users-main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    UsersMainComponent
   ],
   imports: [
     BrowserModule,
